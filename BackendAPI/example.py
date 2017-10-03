edb164d104687d64e7b5cbd9b55411e934a9de5b
seffurSe <shapenov.p@gmail.com>
2017-10-04 00:12:39 +0600

    Добавил readme, логгирование, вынес server_key в конфиг, поправил сохранение и получение сессии устройства

--- BackendAPI/example.py
+++ BackendAPI/example.py
@@ -4,15 +4,24 @@ from abc import ABCMeta, abstractmethod
 from concurrent.futures.thread import ThreadPoolExecutor
 from http import HTTPStatus
 from typing import List
+from logging import getLogger, StreamHandler, INFO
 
+from collections import defaultdict
 from pyfcm import FCMNotification
 from tornado.web import RequestHandler, Application
 from tornado.platform.asyncio import AsyncIOMainLoop
+import lya
 
 
-api_key = 'tornadoapi-a9323'
-push_service = FCMNotification(api_key=api_key)
+cfg = lya.AttrDict.from_yaml('../config.yaml')
+
+push_service = FCMNotification(api_key=cfg.firebase.server_key)
+
 thread_pool_executor = ThreadPoolExecutor(5)
+
+logger = getLogger('tornado')
+logger.addHandler(StreamHandler())
+logger.setLevel(INFO)
 
 
 class ApplicationSession(object):
@@ -38,10 +47,10 @@ class InMemoryApplicationSessionRepositoryImpl(IApplicationSessionRepository):
 
     def __init__(self) -> None:
         super().__init__()
-        self._items_hash = {}
+        self._items_hash = defaultdict(list)
 
     def add(self, application_session: ApplicationSession):
-        self._items_hash[application_session.item] = application_session
+        self._items_hash[application_session.item].append(application_session)
 
     def get_by_items(self, item: str) -> List[ApplicationSession]:
         return self._items_hash[item] if item in self._items_hash else []
@@ -70,8 +79,10 @@ class ApplicationNotificationHandler(RequestHandler):
         applications_sessions = repository.get_by_items(self.get_query_argument("item"))
 
         def go(token: str):
+            logger.info("Notify {} application".format(token))
+
             push_service.notify_single_device(registration_id=token, message_title="New event",
-                                              message_body="New event body")
+                                              message_body="New event body", data_message=dict(payload="payload"))
 
         await gather(*[get_event_loop().run_in_executor(thread_pool_executor, go, application_session.token)
                        for application_session in applications_sessions])
